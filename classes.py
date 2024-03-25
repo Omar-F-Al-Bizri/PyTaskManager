2596415db0bd6fbb9caaf712112a548f04f7c2c7
Omar-F-Al-Bizri <omar.f.bizri@gmail.com>
2024-03-25 15:57:08 +0200

    Added priority queue class, with enqueue, dequeue, and displayQueue methods

--- classes.py
+++ classes.py
@@ -33,3 +33,53 @@ class Task:
         print("Task:", self.__desc, "(id: #" + str(self.__id) + ")")
         print("Priority:", self.__prty)
         print("Completed:", self.__completed)
+
+
+# Priority Queue Class to store Tasks
+class priorityQueue():
+    # Constructor Method
+    def __init__(self):
+        self.__lst = []
+
+    # Add an item with priority
+    def enqueue(self, task : Task):
+        # Case 1: List is empty
+        if len(self.__lst) == 0:
+            self.__lst.append(task)
+        # Case 2: List not empty
+        else:
+            current = 0
+            while self.__lst[current].getPriority() > task.getPriority():
+                # Sub-Case 1: Reached end of List
+                if current == len(self.__lst) - 1:
+                    # Add the item at the end
+                    self.__lst.append(task)
+                    return
+                # Sub-Case 2: Not end of List
+                else:
+                    # Move to the next item in the list
+                    current += 1
+            # Add the item in its position
+            self.__lst.insert(current, task)
+            return
+
+    # Remove item with highest priority
+    def dequeue(self):
+        # Case 1: List is empty
+        if len(self.__lst) == 0:
+            # Inform user that list is empty
+            print("Empty")
+            return
+        # Case 2: List not empty
+        else:
+            # Remove and Return first item in list
+            return self.__lst.pop(0)
+    
+    def displayQueue(self):
+        if len(self.__lst) == 0:
+            print("Empty")
+        else:
+            for i in range(len(self.__lst)):
+                self.__lst[i].displayTask()
+                print()
+        return
