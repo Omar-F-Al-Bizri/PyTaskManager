d4b917882b4c0345b0049f06a833d68f208fe94c
Omar-F-Al-Bizri <omar.f.bizri@gmail.com>
2024-04-01 13:31:20 +0300

    Added textbox to input task description

--- taskmanager_gui.py
+++ taskmanager_gui.py
@@ -8,6 +8,17 @@ def displayTaskManager():
     root.geometry("800x500")
     root.title("Task Manager")
 
+    # Task Input Frame
+    inp_frame = tk.Frame()
+    inp_frame.grid(row=0, column=0, sticky="new")
+    # Input description of task
+    # Label
+    desc_label = tk.Label(inp_frame, text="Description")
+    desc_label.pack()
+    # Input
+    desc_var = tk.StringVar()
+    desc_entry = tk.Entry(inp_frame, textvariable=desc_var)
+    desc_entry.pack()
 
     # Run Display Window
     root.mainloop()
